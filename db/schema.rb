d5791f0408d3866147f9bc902bbac2ad2cf7c88a
EC2 Default User <ec2-user@ip-172-31-42-76.us-east-2.compute.internal>
2020-05-03 18:16:22 +0000

    create Contact Form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200503162020) do
+ActiveRecord::Schema.define(version: 20200503173153) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "email"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
