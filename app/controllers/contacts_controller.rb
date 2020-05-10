52f8f49a5dd463ec424916fc3aa3f71ae193ceef
EC2 Default User <ec2-user@ip-172-31-42-76.us-east-2.compute.internal>
2020-05-10 16:45:06 +0000

    aaa

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,7 +8,7 @@ class ContactsController < ApplicationController
     if @contact.save
       ContactMailer.contact_mail(@contact).deliver
       flash[:success] = 'お問い合わせを受け付けました'
-      redirect_to root_path
+      redirect_to contact_path
     else
       render :new
     end
