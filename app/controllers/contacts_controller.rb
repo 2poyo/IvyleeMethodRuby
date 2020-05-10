f4dde968e4f78eab9d4a04af4dd23da50677abac
EC2 Default User <ec2-user@ip-172-31-42-76.us-east-2.compute.internal>
2020-05-10 16:30:00 +0000

    create mail form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,8 +7,8 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     if @contact.save
       ContactMailer.contact_mail(@contact).deliver
-      flash[:success] = 'Inquiry accepted'
-      redirect_to contact_path
+      flash[:success] = 'お問い合わせを受け付けました'
+      redirect_to root_path
     else
       render :new
     end
